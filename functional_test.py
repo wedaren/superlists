8353d667dcbd103b966292757efcda5b246bc499
wedaren <wedaren@icloud.com>
2016-10-17 20:42:09 +0800

    编辑一个容易理解的故事作为功能测试，也称验收测试（Acceptance Test），端 到端（End to End Test），和黑箱测试（Black box Test）

--- functional_test.py
+++ functional_test.py
@@ -1,11 +1,48 @@
 import time
 from selenium import webdriver
+import unittest
 
-driver = webdriver.Chrome('/Users/wedaren/Downloads/chromedriver')  # Optional argument, if not specified will search path.
-driver.get('http://www.baidu.com/xhtml');
-# time.sleep(5) # Let the user actually see something!
-# search_box = driver.find_element_by_name('q')
-# search_box.send_keys('ChromeDriver')
-# search_box.submit()
-time.sleep(3) # Let the user actually see something!
-driver.quit()
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome('/Users/wedaren/Downloads/chromedriver')  # Optional argument, if not specified will search path.
+        self.browser.get('http://localhost:8000')
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        time.sleep(3) # Let the user actually see something!
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+        # The page updates again, and now shows both items on her list
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
