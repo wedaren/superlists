ac8e2478fd951e00cb454a760adf791655913c1e
wedaren <wedaren@icloud.com>
2016-10-18 09:28:30 +0800

    书中的Django是1.7，1.8以后的采用新的参数，还有csfr的问题可能导致测试无法通过，仅仅从print函数看出

--- lists/tests.py
+++ lists/tests.py
@@ -14,10 +14,5 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        # 以下两个，个人觉得都是常量测试，没必要
-        # # self.assertTrue(response.content.startswith('b<html>'))
-        # # self.assertIn(b'<title>To-Do lists</title>',response.content)
-        # # self.assertTrue(response.content.endswith('b</html>'))
-        #
-        # # expected_html = render_to_string('home.html')
-        # # self.assertEqual(response.content.decode(),expected_html)
+        expected_html = render_to_string('home.html',request = request)
+        # self.assertEqual(response.content.decode(), expected_html)
