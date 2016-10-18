9dd322fca7ffc74a22fc44f9966603d2c3d40895
wedaren <wedaren@icloud.com>
2016-10-18 10:20:49 +0800

    创建一个数据库，测试通过

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
 from django.http import HttpRequest
 from lists.views import home_page
+from lists.models import Item
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -24,3 +25,23 @@ class HomePageTest(TestCase):
 
         response= home_page(request)
         self.assertIn('A new list item',response.content.decode())
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second list item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text,'The first list item')
+        self.assertEqual(second_saved_item.text,'The second list item')
