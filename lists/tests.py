90f80bf330de336e5fbcc68d076c5ba1703b469b
wedaren <wedaren@icloud.com>
2016-10-18 08:20:01 +0800

    第四章基本完事，测试常量是一件很无趣的事，想想法子，可以追加改变吗

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.template.loader import render_to_string
 from django.http import HttpRequest
-
 from lists.views import home_page
 
 # Create your tests here.
@@ -14,6 +14,10 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        # self.assertTrue(response.content.startswith('b<html>'))
-        # self.assertIn(b'<title>To-Do lists</title>',response.content)
-        # self.assertTrue(response.content.endswith('b</html>'))
+        # 以下两个，个人觉得都是常量测试，没必要
+        # # self.assertTrue(response.content.startswith('b<html>'))
+        # # self.assertIn(b'<title>To-Do lists</title>',response.content)
+        # # self.assertTrue(response.content.endswith('b</html>'))
+        #
+        # # expected_html = render_to_string('home.html')
+        # # self.assertEqual(response.content.decode(),expected_html)
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 # Create your views here.
-def home_page(self):
-    return HttpResponse()
+def home_page(request):
+    return render(request,'home.html')
